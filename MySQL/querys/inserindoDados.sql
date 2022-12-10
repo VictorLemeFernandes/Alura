8c47500138880f09f727f79adaa176638fe1d973
Victor Leme <victorlemefernandes@gmail.com>
2022-12-10 10:28:16 -0300

    inserindo e alterando dados

---
+++ MySQL/querys/inserindoDados.sql
@@ -0,0 +1,26 @@
+INSERT INTO tbproduto (
+	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
+) VALUES ('1037797', 'Clean - 2l - Laranja',
+    'lata', '2l', 'Laranja', 16.01);
+    
+INSERT INTO tbproduto (
+	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
+) VALUES ('1000889', 'Sabor da Montanha - 700 ml - Uva',
+    'garrafa', '700 ml', 'Uva', 6.31);
+
+INSERT INTO tbproduto (
+	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
+) VALUES ('1004327', 'Videira do Campo - 1,5 litro - Melancia',
+    'PET', '1,5 litro', 'Melancia', 19.51);
+    
+INSERT INTO tbproduto (
+	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
+) VALUES ('544931', 'Frescor do Verão - 350 ml - Limão',
+	'PET', '350 ml', 'Limão', 3.20);
+
+INSERT INTO tbproduto (
+	PRODUTO, NOME, EMBALAGEM, TAMANHO, SABOR, PRECO_LISTA
+) VALUES ('1078680', 'Frescor do Verão - 470 ml - Manga', 
+	'Lata', '470 ml', 'Manga', 5.18);
+
+SELECT * FROM tbproduto;
