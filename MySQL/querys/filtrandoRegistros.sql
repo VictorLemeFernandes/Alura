4226b098dc5aea60c5a391dce206ca490746f5c3
Victor Leme <victorlemefernandes@gmail.com>
2022-12-14 22:28:40 -0300

    filtrando registros

---
+++ MySQL/querys/filtrandoRegistros.sql
@@ -0,0 +1,7 @@
+SELECT * FROM tbproduto WHERE PRODUTO = '544931';
+
+SELECT * FROM tbcliente WHERE CIDADE = 'rio de janeiro';
+
+SELECT * FROM tbproduto WHERE SABOR = 'citricos';
+
+UPDATE tbproduto SET SABOR = 'Cítricos' WHERE SABOR = 'Limão';
