43d8a25f50dbd4c812b009ee690792a21720da14
Victor Leme <victorlemefernandes@gmail.com>
2022-12-21 19:37:13 -0300

    insercao de dados

---
+++ cursoJava_BancoDados/loja_virtual_repository/loja_virtual_repository/src/TestaInsercao.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercao {
+    public static void main(String[] args) throws SQLException {
+        ConnectionFactory factory = new ConnectionFactory();
+        Connection con = factory.recuperarConexao();
+
+        Statement stm = con.createStatement();
+        stm.execute("INSERT INTO produto (NOME, DESCRICAO) VALUES ('MOUSE', 'MOUSE SEM FIO')"
+        , Statement.RETURN_GENERATED_KEYS);
+
+        ResultSet rst = stm.getGeneratedKeys();
+
+        while(rst.next()) {
+            int id = rst.getInt(1);
+            System.out.println("O id criado foi: " + id);
+        }
+
+    }
+}
