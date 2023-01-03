25f0279c2119b41bbf4afa4bbc070465d0ff246f
Victor Leme <victorlemefernandes@gmail.com>
2023-01-03 15:43:50 -0300

    implementando pool de conexao e datasource

--- cursoJava_BancoDados/loja_virtual_repository/loja_virtual_repository/src/comandosSQL_no_Java/ConnectionFactory.java
+++ cursoJava_BancoDados/loja_virtual_repository/loja_virtual_repository/src/comandosSQL_no_Java/ConnectionFactory.java
@@ -1,17 +1,27 @@
 package comandosSQL_no_Java;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 /*
 * Nessa classe eu faco o encapsulamento da criacao de conexao do BD.
 * */
 public class ConnectionFactory {
+
+    public DataSource dataSource; // dataSource faz com que o pool funcione
+
+    public ConnectionFactory() { // Criando um pool de conexao
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("root");
+
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection recuperarConexao() throws SQLException {
-        return DriverManager.getConnection(
-                "jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-                "root",
-                "root"
-        );
+        return this.dataSource.getConnection();
     }
 }
