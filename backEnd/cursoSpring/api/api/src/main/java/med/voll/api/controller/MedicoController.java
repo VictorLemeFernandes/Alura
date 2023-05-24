a495f0454e982c42496f752594eb2995ac3ec15c
Victor Leme <victorlemefernandes@gmail.com>
2023-05-23 22:03:04 -0300

    atualização de médicos

--- backEnd/cursoSpring/api/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ backEnd/cursoSpring/api/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,10 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,5 +24,11 @@ public class MedicoController {
     @GetMapping
     public List<DadosListagemMedico> listar() {
         return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    }
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        var medico = repository.getReferenceById(dados.id());
+        medico.atualizarInformacoes(dados);
     }
 }
