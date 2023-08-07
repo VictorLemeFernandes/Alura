0fbe378c6f31e45f372ad59fd8a151df37a4f6d2
Victor Leme <victorlemefernandes@gmail.com>
2023-08-07 15:55:07 -0300

    Ajustes na geração do token

--- backEnd/cursoSpring/api/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ backEnd/cursoSpring/api/api/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -3,9 +3,9 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
 import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
 import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,12 +26,13 @@ public class AutenticacaoController {
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-
+        var authenticationTokentoken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         // Objeto que tem o usuario logado
-        var authentication = manager.authenticate(token);
+        var authentication = manager.authenticate(authenticationTokentoken);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
         
-        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 
 }
---
+++ backEnd/cursoSpring/api/api/src/main/java/med/voll/api/infra/security/DadosTokenJWT.java
@@ -0,0 +1,6 @@
+package med.voll.api.infra.security;
+
+// Retorna o token em JSON
+public record DadosTokenJWT(String token) {
+
+}
